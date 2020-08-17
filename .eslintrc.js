00464c9c52c9c4d6bee4ca8a267cc31ad94ebafa
Florent Coriat <florent.coriat@inria.fr>
2020-08-17 17:16:52 +0200

    Setup ESlint and remove TSlint configuration file
    - Remove tslint.json
    - Complete ESlint configuration and move it to .eslintrc.js
    - Configure ESlint not to apply formatting rules
      (to be checked by prettier)

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  plugins: ["react"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "react-app",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/react"
+  ],
+  rules: {
+    "no-console": ["error", { allow: ["warn", "error"] }]
+  }
+};
