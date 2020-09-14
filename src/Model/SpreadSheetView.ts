b4f4d77e6f722ff7d4c7835aa4a5d2758cfccefd
Florent Coriat <florent.coriat@inria.fr>
2020-09-14 10:18:01 +0200

    Fix failing tests
    - client ID had been deliberately removed from code for demo ; put it back
      → /Components/SpreadSheet/Utils.ts : cellIdFromIdString & idStringFromCellId
      Restore commented code (2 lines), removed for BASF
      → Fix tests :
        Create and order row/column ids › makeZeroId
    				      makeIdToTheRight R_0_client and undefined
    				      makeIdBetween R_0 and R_1
    				      id compare
        SpreadSheet Tests		    › empty spreadsheet
    				      Expand table
    - test code did not match its description in
        Create and order row/column ids › makeIdBetween R_0_0 and R_1_0_client
      (test R_1_0 and R_1 → R_1_0_0 instead of R_0_0 and R_1_0 → R_1_0_0)
    - tested indices did not match with provided spreadsheet in
        SpreadSheet Tests		    › add column before
    				      add row after

--- src/Model/SpreadSheetView.ts
+++ src/Model/SpreadSheetView.ts
@@ -1,18 +1,18 @@
 /**
  * MIT License
- * 
+ *
  * Copyright (c) 2020, Concordant and contributors
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -21,18 +21,24 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-import {CellValue, ICellView, ISpreadSheetView} from "../Components/Common/Types/SpreadSheetTypes";
-import {getRowAndColumnFromPos} from "../Components/SpreadSheet/Utils";
+import {
+  CellValue,
+  ICellView,
+  ISpreadSheetView
+} from "../Components/Common/Types/SpreadSheetTypes";
+import { getRowAndColumnFromPos } from "../Components/SpreadSheet/Utils";
 
 type uuid = string;
-type CellIndex = {[id in uuid]: ICellView};
+type CellIndex = { [id in uuid]: ICellView };
 
 export default class SpreadSheetView implements ISpreadSheetView {
   public static DEFAULT_COLUMNS = 5;
   public static DEFAULT_ROWS = 40;
   private readonly cellIndex: CellIndex;
 
-  constructor(public cells: ICellView[][] = new Array(SpreadSheetView.DEFAULT_ROWS)) {
+  constructor(
+    public cells: ICellView[][] = new Array(SpreadSheetView.DEFAULT_ROWS)
+  ) {
     this.cellIndex = {};
     for (const row in this.cells) {
       if (!this.cells[row]) {
@@ -40,7 +46,7 @@ export default class SpreadSheetView implements ISpreadSheetView {
       }
       for (const column in this.cells[0]) {
         if (this.cells[row][column]) {
-          this.cellIndex[this.cells[row][column]!.id] = this.cells[row][column];
+          this.cellIndex[this.cells[row][column].id] = this.cells[row][column];
         }
       }
     }
@@ -53,19 +59,27 @@ export default class SpreadSheetView implements ISpreadSheetView {
         if (!this.cells[row][column]) {
           continue;
         }
-        this.cells[row][column].value = this.replaceIdsWithTableReferences(this.cells[row][column].value);
+        this.cells[row][column].value = this.replaceIdsWithTableReferences(
+          this.cells[row][column].value
+        );
       }
     }
   }
 
   public size() {
     const nRows = this.cells.length;
-    const nColumns = this.cells.reduce((currMax: number, rowCols: any) => (rowCols > currMax ? rowCols : currMax), 0);
-    return {nRows, nColumns};
+    const nColumns = this.cells.reduce(
+      (currMax: number, rowCols: any) =>
+        rowCols > currMax ? rowCols : currMax,
+      0
+    );
+    return { nRows, nColumns };
   }
 
   public contains(row: number, column: number) {
-    return this.cells[row] !== undefined && this.cells[row][column] !== undefined;
+    return (
+      this.cells[row] !== undefined && this.cells[row][column] !== undefined
+    );
   }
 
   public get(row: number, column: number): ICellView {
@@ -77,7 +91,7 @@ export default class SpreadSheetView implements ISpreadSheetView {
   }
 
   public getById(id: any): ICellView {
-    const {row, column} = this.cellIndex[id];
+    const { row, column } = this.cellIndex[id];
     return this.cells[row][column];
   }
 
@@ -94,7 +108,7 @@ export default class SpreadSheetView implements ISpreadSheetView {
 
         const res = this.getById(m[1]);
         if (res != null) {
-          const {row: r, column: c} = res;
+          const { row: r, column: c } = res;
           vs = vs.replace(m[0], getRowAndColumnFromPos(r, c));
         }
 
